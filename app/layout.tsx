a619cf9b370c1c1a72e4a6f49de3cce2a0af630c
Nona Dzhurkova <nona.dzhurkova@hotmail.com>
2025-01-03 16:26:36 +0200

    Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import CookieBanner from "../components/cookie";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -113,6 +114,7 @@ export default function RootLayout({
         {children}
         <Footer />
         <CookieBanner />
+        <Analytics />
       </body>
     </html>
   );
